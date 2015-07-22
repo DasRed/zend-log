ecdfdf8ad4cb84f1541ae72dcb519be4f2758515
Marco Starker <marco.starker@myposter.de>
2015-07-22 09:15:26 +0200

    zend log to console

---
+++ src/Log/Writer/Console.php
@@ -0,0 +1,133 @@
+<?php
+namespace DasRed\Zend\Log\Writer;
+
+use Zend\Log\Logger;
+use Zend\Log\Writer\AbstractWriter;
+use Zend\Console\Adapter\AdapterInterface;
+
+class Console extends AbstractWriter
+{
+	const QUIT = -1;
+
+	/**
+	 *
+	 * @var AdapterInterface
+	 */
+	protected $console;
+
+	/**
+	 *
+	 * @var int
+	 */
+	protected $level = Logger::ERR;
+
+	/**
+	 *
+	 * @param AdapterInterface $console
+	 * @param int $level
+	 * @param string $options
+	 */
+	public function __construct(AdapterInterface $console, $level = Logger::ERR, $options = null)
+	{
+		$this->setConsole($console)->setLevel($level);
+
+		parent::__construct($options);
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 *
+	 * @see \Zend\Log\Writer\AbstractWriter::doWrite()
+	 */
+	protected function doWrite(array $event)
+	{
+		if ($event['priority'] > $this->getLevel())
+		{
+			return;
+		}
+
+		$this->getConsole()->writeLine($event['message']);
+	}
+
+	/**
+	 *
+	 * @return AdapterInterface
+	 */
+	public function getConsole()
+	{
+		return $this->console;
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getLevel()
+	{
+		return $this->level;
+	}
+
+	/**
+	 *
+	 * @param AdapterInterface $console
+	 * @return self
+	 */
+	public function setConsole(Console $console)
+	{
+		$this->console = $console;
+
+		return $this;
+	}
+
+	/**
+	 *
+	 * @param int $level
+	 * @return self
+	 */
+	public function setLevel($level)
+	{
+		$this->level = (int)$level;
+
+		return $this;
+	}
+
+	/**
+	 * @return self
+	 */
+	public function setQuit()
+	{
+		$this->setLevel(self::QUIT);
+
+		return $this;
+	}
+
+	/**
+	 *
+	 * @param int $verboseLevel
+	 * @return self
+	 */
+	public function setVerboseLevel($verboseLevel)
+	{
+		switch ($verboseLevel)
+		{
+			case 0:
+				$this->setLevel(Logger::ERR);
+				break;
+
+			case 1:
+				$this->setLevel(Logger::WARN);
+				break;
+
+			case 2:
+				$this->setLevel(Logger::INFO);
+				break;
+
+			case 3:
+			default:
+				$this->setLevel(Logger::DEBUG);
+				break;
+		}
+
+		return $this;
+	}
+}
